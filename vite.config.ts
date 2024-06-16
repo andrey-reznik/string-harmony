d35b651bd91516d81a7ebc5d20b61c3b83c86817
Andrey Reznik <andrey.reznik@yahoo.com>
2024-06-16 09:32:02 +0300

    fix: Build only es module

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 export default defineConfig({
-  esbuild: {
-    format: 'cjs',
-  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'string-harmony',
-      fileName: (format) => `index.${format}.js`,
+      entry: resolve(__dirname, 'src/index.ts'), // Path to your entry file
+      name: 'StringHarmony',
+      formats: ['es'], // Output format as ESM
+      fileName: 'index',
     },
-    sourcemap: true,
-    emptyOutDir: true,
+    rollupOptions: {
+      external: ['node:fs'],
+      output: {
+        globals: {
+          'node:fs': 'fs',
+        },
+      },
+    },
   },
-  plugins: [dts()],
+  plugins: [
+    dts(), // Plugin to generate .d.ts files
+  ],
 })
